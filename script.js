273dee8c72c264a368512cc9a14d811e0e192272
randyluna <135148119+randyluna@users.noreply.github.com>
2023-07-12 18:10:54 -0400

    added class ship and attack target

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//  Earth has been attacked by aliens. You are the captain on a mission to destory every alien ship. 
-// There are 6 aliens. They attack one at a time. You can only attack the aliens in order. After you destroyed a ship, you have the option to retreat 
-
-// You attack the first ship. If the ship survives then it can attack you. If you destory the ship you have the option to attack the next ship or to retreat.
-//  You win if all alients are destoryed. You lose the game if you are destroyed.  
 
 // Ship Properties
 // hull is the same as hitpoints. If hull reaches 0or less, the ship is destroyed
@@ -20,27 +15,37 @@
 // accuracy - between .6and .8
 // You could be battling six alien ships each with unique values.
 
-const ussAssembly=(
-    hull:20; 
-    firepower:5;
-    accuracy:.7;
-    isAlive:true;
-)
+// Ship Class 
 
-// const alien={
-
-// }
-
-this.hull=Math.floor(Math.random()*4)+3;
-this.firepower=Math.floor(Math.random()*3)+2;
-this.accuracy= (Math.floor(Math.random()*3)+6)/10;
-
-class ship{
-    constructor(name,hull,firepower,accuracy){
-        this.name= name; 
+class spaceShip{
+    constructor(hull,firepower,accuracy){
         this.hull= hull;
         this.firepower= firepower;
         this.accuracy=accuracy;
     }
 }
+
+attack(target){
+    if (Math.random()<= this.accuracy){
+        target.hull -=this.firepower;
+        console.log ("Ship Hit!", target.hull);
+    }else{
+    console.log ("You missed the target!");
+    }
+}
+
+shipDestroyed(){
+    return this.hull <=0;
+}
+// My ship
+const ussAssemble=(20,5,0.7)
+
+// Alien Ships
+const alienShips[]; for (let i=0; i<6, i++)
+
+//  Alien Ship ranged properties 
+this.hull=Math.floor(Math.random()*4)+3;
+this.firepower=Math.floor(Math.random()*3)+2;
+this.accuracy= (Math.floor(Math.random()*3)+6)/10;
+
 const ussAssembly= ussAssembly
